ec08c21a975435dc42686e22507095e8e12d227e
edouard emberger <edouembe@A201-PC8.ensea.fr>
2014-12-02 10:48:00 +0100

    chessboard

--- GUI/ClickableElement.cpp
+++ GUI/ClickableElement.cpp
@@ -2,11 +2,30 @@
 
 class ClickableELement : public GraphicElement{
 	private:
-		
+		Point2I pointDebut, pointFin;
 	
 	public:
+		ClickableELement():GraphicElement();
+		virtual void setStartPoint(Point2I p)
+		void setEndPoint(Point2I p)
 		void pressed();
 		void clicked();
 		void released();
 		void wheeled();
 }
+
+public ClickableELement::ClickableELement(){
+
+}
+
+public void ClickableELement::setStartPoint(Point p){
+	pointDebut.x = p.x;
+	pointDebut.y = p.y;
+}
+
+public void ClickableELement::setEndPoint(Point p){
+	pointFin.x = p.x;
+	pointFin.y = p.y;
+}
+
+
