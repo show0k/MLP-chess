7221c65d6195b9dfe6d74a48efa5518db9e552a7
Dood <edouard.emberger@gmail.com>
2014-12-31 22:38:45 +0100

    fin GUI

--- GUI/main.cpp
+++ GUI/main.cpp
@@ -1,15 +1,16 @@
 #include "Chess.hpp"
 
 int main(int argc,char  **argv){
-
+	bool check = true;
 	sf::Thread thread(&GUI);
 	thread.launch(); 
 	string s ="";
-	while(s != "exit"){
+	while(stringContains(s,"exit")){
 		s = getResult();
 		parseAction(s);
 	}
-
+	//cout<<"ici et la"<<endl;
+	thread.wait ();
 	return EXIT_SUCCESS;
 }
 
