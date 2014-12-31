899464b501d9fbe09e13890a330c016eee4b59b5
Théo Segonds <showok@showok.info>
2014-12-31 13:55:03 +0100

    maj run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,15 @@
 #!/bin/bash   
-echo 'Start the Chess program'
+echo 'Start the Chess program. Enjoy !'
 
 # On redirige la sortie de l'engine sur l'entrée du la GUI,et inversement
 
+cd GUI/
+rm fifo* 2>/dev/null
 mkfifo fifo0 fifo1
-( ./engine/chessEngine.out > fifo0 < fifo1 ) &
-( ./GUI/GUI.out > fifo1 < fifo0 ) &
-( exec 30<fifo0 31<fifo1 )   
 
+./../engine/chessEngine.out < fifo0 | ./GUI.out  > fifo0
+#( ./../engine/chessEngine.out > fifo0 > log_engine.log < fifo1 ) &
+#( ./GUI.out > fifo1 > log_gui.log < fifo0 ) &
+#( exec 30<fifo0 31<fifo1 )   
 
+trap "echo 'Chess program closed.'" SIGINT SIGTERM SIGKILL
