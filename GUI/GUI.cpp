94959a4da90398a1ff136d4deda469d418227e21
Dood <edouard.emberger@gmail.com>
2014-12-31 16:11:20 +0100

    Fermeture

--- GUI/GUI.cpp
+++ GUI/GUI.cpp
@@ -22,8 +22,10 @@ void GUI(){
 		sf::Event event;
 		while (window.pollEvent(event)){
 		
-			if (event.type == sf::Event::Closed)
+			if (event.type == sf::Event::Closed){
 				window.close();
+				exit(0);
+			}
 			else if (event.type == sf::Event::MouseButtonPressed ){
 				notifyInterface(event);
 				if(gameGoesOn)
